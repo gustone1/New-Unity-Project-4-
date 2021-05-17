9861694691e828800a64bfe10bf1fd0d87ef3e9a
gust <elisumix@gmail.com>
2021-05-17 17:12:19 +0200

    Initial commit

---
+++ Assets/CloneZone.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneZone : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        Instantiate(other.gameObject);
+    }
+}
